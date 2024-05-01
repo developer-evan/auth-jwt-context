9c523fec5c39698fef0731be9cf8ada3b8cf2f57
Mogeni <evansmogeni254@gmail.com>
2024-05-01 21:07:20 +0300

    commit

--- app/screens/Home.tsx
+++ app/screens/Home.tsx
@@ -1,26 +1,23 @@
-import { ScrollView, StyleSheet, Text, View } from 'react-native'
-import React, { useEffect, useState } from 'react'
-import axios from 'axios';
-import { API_URL } from '../context/AuthContext';
+import { ScrollView, StyleSheet, Text, View } from "react-native";
+import React, { useEffect, useState } from "react";
+import axios from "axios";
+import { API_URL } from "../context/AuthContext";
 
 const Home = () => {
-    const [users, setUsers] = useState<any[]>([]);
-
-
-    useEffect(() => {
-        const loadUser = async () => {
-            try {
-            const result = await axios.get(`${API_URL}/users`);
-            console.log(result.data);
-            setUsers(result.data);
-        }catch (e: any) {
-            // console.log(e);
-            alert(e.message)
-        }
-    }
-        loadUser();
-    }, []);
-
+  const [users, setUsers] = useState<any[]>([]);
+  useEffect(() => {
+    const loadUser = async () => {
+      try {
+        const result = await axios.get(`${API_URL}/users`);
+        console.log(result.data);
+        setUsers(result.data);
+      } catch (e: any) {
+        console.log(e);
+        alert(e.message);
+      }
+    };
+    loadUser();
+  }, []);
 
   return (
     <ScrollView>
@@ -29,9 +26,9 @@ const Home = () => {
         <Text key={user.id}>{user.email}</Text>
       ))}
     </ScrollView>
-  )
-}
+  );
+};
 
-export default Home
+export default Home;
 
-const styles = StyleSheet.create({})
+const styles = StyleSheet.create({});
